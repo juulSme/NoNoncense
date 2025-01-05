711b1249e6f3561bb2a3976a63432baed0ee0d6d
J. Smeets <16883019+juulSme@users.noreply.github.com>
2025-01-05 15:55:08 +0100

    Remove hello world stuff

--- lib/no_noncense.ex
+++ lib/no_noncense.ex
@@ -2,17 +2,4 @@ defmodule NoNoncense do
   @moduledoc """
   Documentation for `NoNoncense`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> NoNoncense.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/no_noncense_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule NoNoncenseTest do
-  use ExUnit.Case
-  doctest NoNoncense
-
-  test "greets the world" do
-    assert NoNoncense.hello() == :world
-  end
-end
