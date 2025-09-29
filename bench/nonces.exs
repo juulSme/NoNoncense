43b9b4ecc1cecaf3929edb2e0723e224e9eba09c
Julien Smeets <16883019+juulSme@users.noreply.github.com>
2025-09-29 21:20:06 +0200

    Benchmark using 4 threads

--- bench/nonces.exs
+++ bench/nonces.exs
@@ -1,3 +1,4 @@
+tasks = 4
 NoNoncense.init(machine_id: 0)
 
 # artifially set back the clock, so that 64-bits nonces don't hit their time-based rate limit
@@ -10,29 +11,32 @@ key192 = :crypto.strong_rand_bytes(24)
 
 %{
   "strong_rand_bytes(8) single" => [fn -> :crypto.strong_rand_bytes(8) end, tasks: 1],
-  "strong_rand_bytes(8) multi" => [fn -> :crypto.strong_rand_bytes(8) end],
+  "strong_rand_bytes(8) multi" => [fn -> :crypto.strong_rand_bytes(8) end, tasks: tasks],
   "strong_rand_bytes(12) single" => [fn -> :crypto.strong_rand_bytes(12) end, tasks: 1],
-  "strong_rand_bytes(12) multi" => [fn -> :crypto.strong_rand_bytes(12) end],
+  "strong_rand_bytes(12) multi" => [fn -> :crypto.strong_rand_bytes(12) end, tasks: tasks],
   "strong_rand_bytes(16) single" => [fn -> :crypto.strong_rand_bytes(16) end, tasks: 1],
-  "strong_rand_bytes(16) multi" => [fn -> :crypto.strong_rand_bytes(16) end],
+  "strong_rand_bytes(16) multi" => [fn -> :crypto.strong_rand_bytes(16) end, tasks: tasks],
   "nonce(64) single" => [fn -> NoNoncense.nonce(64) end, tasks: 1],
-  "nonce(64) multi" => [fn -> NoNoncense.nonce(64) end],
+  "nonce(64) multi" => [fn -> NoNoncense.nonce(64) end, tasks: tasks],
   "nonce(96) single" => [fn -> NoNoncense.nonce(96) end, tasks: 1],
-  "nonce(96) multi" => [fn -> NoNoncense.nonce(96) end],
+  "nonce(96) multi" => [fn -> NoNoncense.nonce(96) end, tasks: tasks],
   "nonce(128) single" => [fn -> NoNoncense.nonce(128) end, tasks: 1],
-  "nonce(128) multi" => [fn -> NoNoncense.nonce(128) end],
+  "nonce(128) multi" => [fn -> NoNoncense.nonce(128) end, tasks: tasks],
   "encrypted_nonce(64) single" => [fn -> NoNoncense.encrypted_nonce(64, key192) end, tasks: 1],
-  "encrypted_nonce(64) multi" => [fn -> NoNoncense.encrypted_nonce(64, key192) end],
+  "encrypted_nonce(64) multi" => [fn -> NoNoncense.encrypted_nonce(64, key192) end, tasks: tasks],
   "encrypted_nonce(96) single" => [fn -> NoNoncense.encrypted_nonce(96, key192) end, tasks: 1],
-  "encrypted_nonce(96) multi" => [fn -> NoNoncense.encrypted_nonce(96, key192) end],
+  "encrypted_nonce(96) multi" => [fn -> NoNoncense.encrypted_nonce(96, key192) end, tasks: tasks],
   "encrypted_nonce(128) single" => [fn -> NoNoncense.encrypted_nonce(128, key128) end, tasks: 1],
-  "encrypted_nonce(128) multi" => [fn -> NoNoncense.encrypted_nonce(128, key128) end],
+  "encrypted_nonce(128) multi" => [
+    fn -> NoNoncense.encrypted_nonce(128, key128) end,
+    tasks: tasks
+  ],
   "sortable_nonce(64) single" => [fn -> NoNoncense.sortable_nonce(64) end, tasks: 1],
-  "sortable_nonce(64) multi" => [fn -> NoNoncense.sortable_nonce(64) end],
+  "sortable_nonce(64) multi" => [fn -> NoNoncense.sortable_nonce(64) end, tasks: tasks],
   "sortable_nonce(96) single" => [fn -> NoNoncense.sortable_nonce(96) end, tasks: 1],
-  "sortable_nonce(96) multi" => [fn -> NoNoncense.sortable_nonce(96) end],
+  "sortable_nonce(96) multi" => [fn -> NoNoncense.sortable_nonce(96) end, tasks: tasks],
   "sortable_nonce(128) single" => [fn -> NoNoncense.sortable_nonce(128) end, tasks: 1],
-  "sortable_nonce(128) multi" => [fn -> NoNoncense.sortable_nonce(128) end]
+  "sortable_nonce(128) multi" => [fn -> NoNoncense.sortable_nonce(128) end, tasks: tasks]
 }
 |> Benchmark.bench_many()
 |> Benchmark.format_results()
